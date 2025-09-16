8699fda3a93ed5be606a0f76941ff0dc19218a6e
wanwisasin <Wanwisa_j@cmu.ac.th>
2025-09-16 22:23:56 +0700

    deploy

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   charset: 'utf8mb4',   
 });
 
